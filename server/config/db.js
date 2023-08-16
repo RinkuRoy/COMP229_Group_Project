49b04153701c705ce5afeaf050aaa9c697d223e6
RinkuRoy <rinkumroy@gmail.com>
2023-08-15 23:08:01 -0400

    Initial commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+    //"URI": "mongodb+srv://admin:admin@mongodbserver.lkz5jhf.mongodb.net/grocery_store?retryWrites=true&w=majority"
+    "URI": "mongodb://127.0.0.1:27017/foodspot"
+}
